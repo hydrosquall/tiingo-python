c3394c8cdc81fbb895d35fe85066c256531c2eb4
n1rna <n7r.distributed.ai@gmail.com>
2019-10-18 19:06:35 +0330

    Add support for multi tickers in list_tickers function

--- tests/test_tiingo.py
+++ tests/test_tiingo.py
@@ -108,6 +108,21 @@ class TestTickerPrices(TestCase):
         self.assertGreater(len(prices), 1)
 
     @vcr.use_cassette('tests/fixtures/list_stock_tickers.yaml')
+    def test_list_all_stock_tickers(self):
+        tickers = self._client.list_tickers()
+        assert len(tickers) > 1
+        assert any(ticker['assetType' == 'Stock' for ticker in tickers)
+        assert any(ticker['assetType' == 'Mutual Fund' for ticker in tickers)
+        assert any(ticker['assetType' == 'ETF' for ticker in tickers)
+
+    def test_list_multi_stock_tickers(self):
+        tickers = self._client.list_tickers(['Stock', 'ETF'])
+        assert len(tickers) > 1
+        assert any(ticker['assetType' == 'Stock' for ticker in tickers)
+        assert any(ticker['assetType' == 'ETF' for ticker in tickers)
+        assert all(ticker['assetType' != 'Mutual Fund' for ticker in tickers)
+
+    @vcr.use_cassette('tests/fixtures/list_stock_tickers.yaml')
     def test_list_stock_tickers(self):
         tickers = self._client.list_stock_tickers()
         assert len(tickers) > 1
