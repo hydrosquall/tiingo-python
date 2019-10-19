6e36317961d97e4b237a8028b0c68c5aecfcd73d
Cameron Yick <cameron.yick@datadoghq.com>
2019-10-18 23:25:45 -0400

    [tests] Address typos in cassette test imports

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -14,23 +14,22 @@ class TestCryptoEndpoints(TestCase):
     def setUp(self):
         self._client = TiingoClient()
 
-    @vcr.user_cassette('tests/fixtures/crypto_metadata.yaml')
+    @vcr.use_cassette('tests/fixtures/crypto_metadata.yaml')
     def test_crypto_metadata(self):
-        metadata = self._client.get_crypto_metadata(ticker=['btcusd', 'fldcbtc'])
+        metadata = self._client.get_crypto_metadata(tickers=['btcusd', 'fldcbtc'])
 
         assert len(metadata) == 2
-        assert metadata[0].ticker == 'btcusd'
-        assert metadata[1].ticker == 'fldcbtc'
+        assert metadata[0]['ticker'] == 'btcusd'
+        assert metadata[1]['ticker'] == 'fldcbtc'
 
     @vcr.use_cassette('tests/fixtures/crypto_top_of_book.yaml')
     def test_crypto_top_of_book(self):
-        top_of_book = self._client.get_crypto_top_of_book(tickers=['btcusd', 'fldcbtc'],
-                                                          endDate='2018-01-02', includeRawExchangeData=True)
+        top_of_book = self._client.get_crypto_top_of_book(tickers=['btcusd', 'fldcbtc'], includeRawExchangeData=True)
         assert 'topOfBookData' in top_of_book
         assert len(top_of_book['topOfBookData']) == 2
         assert 'exchangeData' in top_of_book
 
-    @vcr.user_cassette('tests/fixtures/crypto_price_history.yaml')
+    @vcr.use_cassette('tests/fixtures/crypto_price_history.yaml')
     def test_crypto_price_history(self):
         price_history = self._client.get_crypto_price_history(tickers=['btcusd', 'fldcbtc'],
                                                               startDate='2019-05-01', endDate='2019-05-02',
